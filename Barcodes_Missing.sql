0c6308eb5c03b73fc19f9b59131addc00e805091
Rafał <rpapierkowski@gmail.com>
2023-03-22 13:41:20 +0100

    Create Barcodes_Missing.sql

---
+++ Barcodes_Missing.sql
@@ -0,0 +1,53 @@
+-- Barcodes Wietzendorf
+SELECT 
+	bd.barcode AS Barcode
+,	vb.ware_loc AS 'Warehouse location'
+,	w.name AS 'Last Warehouse'
+
+FROM vbarcode vb
+
+JOIN barcode_dn bd ON bd.id = vb.id
+
+LEFT JOIN warehouse w ON w.warehouse_id = bd.last_warehouse_id
+
+WHERE 
+LEFT(vb.ware_loc,1) = "W"
+AND LEFT(vb.ware_loc,2) NOT IN ( "WW" , "Wa")
+AND bd.last_warehouse_id NOT IN (115, 134)
+
+
+UNION ALL
+
+-- Barcodes Rajkowo
+SELECT 
+	bd.barcode AS Barcode
+,	vb.ware_loc AS 'Warehouse location'
+,	w.name AS 'Last Warehouse'
+
+FROM vbarcode vb
+
+JOIN barcode_dn bd ON bd.id = vb.id
+
+LEFT JOIN warehouse w ON w.warehouse_id = bd.last_warehouse_id
+
+WHERE 
+LEFT(vb.ware_loc,2) = "R1"
+AND bd.last_warehouse_id NOT IN (107, 132)
+
+UNION ALL 
+
+-- Barcodes Lager Däniken
+SELECT 
+	bd.barcode AS Barcode
+,	vb.ware_loc AS 'Warehouse location'
+,	w.name AS 'Last Warehouse'
+
+FROM vbarcode vb
+
+JOIN barcode_dn bd ON bd.id = vb.id
+
+LEFT JOIN warehouse w ON w.warehouse_id = bd.last_warehouse_id
+
+WHERE 
+LEFT(vb.ware_loc,2) = "D1"
+AND bd.last_warehouse_id NOT IN (116, 133)
