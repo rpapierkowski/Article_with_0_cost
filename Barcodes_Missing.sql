07d41b13bf2fbeb928a35cf6d10e2752bc45978a
Rafał <rpapierkowski@gmail.com>
2023-03-23 14:52:40 +0100

    Update Barcodes_Missing.sql

--- Barcodes_Missing.sql
+++ Barcodes_Missing.sql
@@ -1,8 +1,9 @@
 -- Barcodes Wietzendorf
 SELECT 
 	bd.barcode AS Barcode
-,	vb.ware_loc AS 'Warehouse location'
 ,	w.name AS 'Last Warehouse'
+,	vb.ware_loc AS 'Warehouse Location'
+,	w2.name AS 'Location Warehouse Name'
 
 FROM vbarcode vb
 
@@ -10,19 +11,25 @@ JOIN barcode_dn bd ON bd.id = vb.id
 
 LEFT JOIN warehouse w ON w.warehouse_id = bd.last_warehouse_id
 
-WHERE 
-LEFT(vb.ware_loc,1) = "W"
-AND LEFT(vb.ware_loc,2) NOT IN ( "WW" , "Wa")
-AND bd.last_warehouse_id NOT IN (115, 134)
-AND vb.inactive = 0
+LEFT JOIN warehouse_cells wc ON bd.warehouse_cell_id = wc.id 
 
-UNION ALL
+LEFT JOIN ware_la wl ON wc.ware_la_id = wl.id
+
+LEFT JOIN warehouse w2 ON w2.warehouse_id = wl.warehouse_id
+
+WHERE 
+vb.inactive = 0
+AND bd.last_warehouse_id NOT IN (115, 134)
+AND wl.warehouse_id IN (115, 134)
+
+UNION ALL 
 
 -- Barcodes Rajkowo
 SELECT 
 	bd.barcode AS Barcode
-,	vb.ware_loc AS 'Warehouse location'
 ,	w.name AS 'Last Warehouse'
+,	vb.ware_loc AS 'Warehouse Location'
+,	w2.name AS 'Location Warehouse Name'
 
 FROM vbarcode vb
 
@@ -30,18 +37,25 @@ JOIN barcode_dn bd ON bd.id = vb.id
 
 LEFT JOIN warehouse w ON w.warehouse_id = bd.last_warehouse_id
 
+LEFT JOIN warehouse_cells wc ON bd.warehouse_cell_id = wc.id 
+
+LEFT JOIN ware_la wl ON wc.ware_la_id = wl.id
+
+LEFT JOIN warehouse w2 ON w2.warehouse_id = wl.warehouse_id
+
 WHERE 
-LEFT(vb.ware_loc,2) = "R1"
+vb.inactive = 0
 AND bd.last_warehouse_id NOT IN (107, 132)
-AND vb.inactive = 0
+AND wl.warehouse_id IN (107, 132)
 
 UNION ALL 
 
 -- Barcodes Lager Däniken
 SELECT 
 	bd.barcode AS Barcode
-,	vb.ware_loc AS 'Warehouse location'
 ,	w.name AS 'Last Warehouse'
+,	vb.ware_loc AS 'Warehouse Location'
+,	w2.name AS 'Location Warehouse Name'
 
 FROM vbarcode vb
 
@@ -49,7 +63,14 @@ JOIN barcode_dn bd ON bd.id = vb.id
 
 LEFT JOIN warehouse w ON w.warehouse_id = bd.last_warehouse_id
 
+LEFT JOIN warehouse_cells wc ON bd.warehouse_cell_id = wc.id 
+
+LEFT JOIN ware_la wl ON wc.ware_la_id = wl.id
+
+LEFT JOIN warehouse w2 ON w2.warehouse_id = wl.warehouse_id
+
 WHERE 
-LEFT(vb.ware_loc,2) = "D1"
+vb.inactive = 0
 AND bd.last_warehouse_id NOT IN (116, 133)
-AND vb.inactive = 0
+AND wl.warehouse_id IN (116, 133)
+
